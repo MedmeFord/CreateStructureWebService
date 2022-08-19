bc06b070c27d90bf61aeb913f4344d39622b16e1
parkinso <89875845827@mail.ru>
2022-08-19 16:02:19 +0300

    BeforeAddDB

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,26 +4,34 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
-
 	addr := flag.String("addr", ":4000", "Сетевой адрес HTTP")
-
 	flag.Parse()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	infoLog := log.New(os.Stdout, "INFO\t", log.LUTC|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.LUTC|log.Ltime|log.Llongfile)
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static", http.NotFoundHandler())
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 
-	log.Printf("Запуск сервера на %s", *addr)
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+	infoLog.Printf("Запуск сервера на %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
 
 // type neuteredFileSystem struct {
