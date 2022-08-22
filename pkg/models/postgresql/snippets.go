d7521ba0df838eb8922a677ea754a52d0fb9970d
parkinso <89875845827@mail.ru>
2022-08-22 11:09:59 +0300

    ReturnIdFromTable

--- pkg/models/postgresql/snippets.go
+++ pkg/models/postgresql/snippets.go
@@ -10,7 +10,30 @@ type SnippetModel struct {
 }
 
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	// Ниже будет SQL запрос, который мы хотим выполнить. Мы разделили его на две строки
+	// для удобства чтения (поэтому он окружен обратными кавычками
+	// вместо обычных двойных кавычек).
+	// str := []rune(expires) + 'year'
+	// str = append(str, 'year')
+
+	// stmt := fmt.Sprintf("INSERT INTO snippets (title, content_, created, expires)VALUES(%s, %s,  current_timestamp, current_timestamp + interval '%s years", title, content, expires)
+
+	// stmt := `INSERT INTO snippets (title, content_, created, expires)
+	// VALUES($1, $2, current_timestamp, current_timestamp + $3::interval)`
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+		VALUES($1, $2, current_timestamp, current_timestamp + interval '1 year' * $3)`
+	_, err := m.DB.Exec(stmt, title, content, expires) // много вопросов
+	if err != nil {
+		return 0, err
+	}
+	// Используем метод LastInsertId(), чтобы получить последний ID
+	// созданной записи из таблицу snippets.
+	var id int
+	err = m.DB.QueryRow("INSERT INTO snippets (title) VALUES ('John') RETURNING id").Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+	return id, err
 }
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
